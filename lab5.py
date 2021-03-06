f33afc02024f69f49dd0d089789992d1e1748266
lunappleseed <71196946+lunappleseed@users.noreply.github.com>
2021-03-06 14:50:19 -0500

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -33,7 +33,115 @@ def BFS(node):
 def get_all_nodes(node):
     '''Return a list of the nodes in the graph of nodes connected to node
     (N.B., the nodes can be indirectly connected as well)'''
+    list = []
+    q = [node]
+    node.visited = True
+    while len(q) > 0:
+        cur = q.pop(0)
+        list.append(cur.name)
+        for con in cur.connections:
+            if not con["node"].visited:
+                q.append(con["node"])
+                con["node"].visited = True
+    print(list)
+################################################################################
 
+def unvisit_all(node):
+    '''Change all n.visited to False in all the nodes in the graph of nodes
+    connected to node. Use BFS to find all the nodes'''
+    q = [node]
+    node.visited = False
+    while len(q) > 0:
+        cur = q.pop(0)
+        for con in cur.connections:
+            if con["node"].visited:
+                q.append(con["node"])
+                con["node"].visited = False
+
+
+###############################################################################
+
+def DFS_rec(node):
+    '''Print out the names of all nodes connected to node using a
+    recursive version of DFS'''
+    node.visited = True
+    print(node.name)
+    for con in node.connections:
+        if not con["node"].visited:
+            DFS_rec(con["node"])
+
+################################################################################
+
+def DFS_nonrec(node):
+    '''Print out the names of all nodes connected to node using a non-recursive
+    version of DFS. Make it so that the nodes are printed in the same order
+    as in DFS_rec'''
+    q = [node]
+    node.visited = True
+    while len(q) > 0:
+        cur = q.pop()
+        print(cur.name)
+        for con in cur.connections:
+            if not con["node"].visited:
+                q.append(con["node"])
+                con["node"].visited = True
+
+################################################################################
+#
+# OPTIONAL
+#
+
+def dijsktra_slowish(node):
+    '''Implement Dijkstra's algorithm as discussed in class (i.e., you don't
+    need to use a priority queue'''
+    S = [node]
+    d = {node.name: 0}
+
+    unexplored = get_all_nodes(node)
+    unexplored.remove(node)
+
+    while len(unexplored) > 0:
+        # your code here
+        pass
+
+    return d
+
+def dijkstra_pq(node):
+    d = {}  # d[node.name] is the length of the shortest path from node to node_name
+    pq = [(0, node)]  # the first thing that will be popped from pq
+    while len(pq) > 0:  # keep adding nodes
+        cur_dist, cur_node = heapq.heappop(pq)
+        if cur_node.name in d:
+            continue
+
+        d[cur_node.name] = cur_dist
+        for con in cur_node.connections:
+            dist = con["weight"] + cur_dist
+            node = con["node"]
+            heapq.heappush(pq, (dist, node))
+
+    return d
+
+
+if __name__ == '__main__':
+    TO = Node("TO")
+    NYC = Node("NYC")
+    DC = Node("DC")
+    CDMX = Node("CDMX")
+    SF = Node("SF")
+
+    connect(TO, NYC, 3)
+    connect(TO, SF, 6)
+    connect(TO, CDMX, 7)
+    connect(NYC, DC, 2)
+    connect(SF, DC, 5)
+
+    # L = get_all_nodes(TO)
+    # DFS(TO)
+    # #DFS_rec(TO)
+    # unvisit_all(TO)
+    # DFS(TO)
+    # print(dijsktra_slowish(TO))
 ################################################################################
 
 def unvisit_all(node):
